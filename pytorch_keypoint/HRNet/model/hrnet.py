0d1a8ad7000b216bf98e0a3a8adbebf249006819
hoewonZ <1488767@qq.com>
2022-12-18 13:53:17 +0800

    Update hrnet.py

--- pytorch_keypoint/HRNet/model/hrnet.py
+++ pytorch_keypoint/HRNet/model/hrnet.py
@@ -2,12 +2,16 @@ import torch.nn as nn
 
 BN_MOMENTUM = 0.1
 
-
+'''
+BasicBlock 简单理解是2次卷积块。网络的stage阶段基本不会下采样。看着就是该通道数.比bottleneck少一层卷积。
+cov2d-bn-relu-cov2d-bn-(downsample)-relu，downsample具体看出入的卷积是什么样的。
+'''
 class BasicBlock(nn.Module):
     expansion = 1
 
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
+        # k=3 s=1,p=1 不会下采样，会维持原分辨率
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
         self.relu = nn.ReLU(inplace=True)
@@ -34,10 +38,18 @@ class BasicBlock(nn.Module):
 
         return out
 
-
+'''
+Bottleneck主要是3次1*1卷积。其中downsample可能会改变通道数，具体看传入的模型。
+'''
 class Bottleneck(nn.Module):
     expansion = 4
-
+    '''
+        作用：3+1个1*1卷积，如果有downsample，会增加通道数。前两个conv2d一直做1*1，因此尺寸不变，第三个conv2d通道数改变为4倍。
+        params:
+            inplanes: input channel num
+            planes：output channel num
+            downsample: 传入1x1的卷积,用来调整通道数
+    '''
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
@@ -45,6 +57,7 @@ class Bottleneck(nn.Module):
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
                                padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
+        # 下面这个conv3通道数扩大了4倍
         self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1,
                                bias=False)
         self.bn3 = nn.BatchNorm2d(planes * self.expansion,
@@ -70,7 +83,7 @@ class Bottleneck(nn.Module):
         if self.downsample is not None:
             residual = self.downsample(x)
 
-        out += residual
+        out += residual # 广播机制，out和residual的通道数不同，out会扩展到与residual相同的通道数然后相加特征值，如H*W*64 + H*W*256，最后结果为H*W*256
         out = self.relu(out)
 
         return out
@@ -100,41 +113,44 @@ class StageModule(nn.Module):
             self.branches.append(branch)
 
         self.fuse_layers = nn.ModuleList()  # 用于融合每个分支上的输出
-        for i in range(self.output_branches):
+        for ob in range(self.output_branches):
             self.fuse_layers.append(nn.ModuleList())
-            for j in range(self.input_branches):
-                if i == j:
+            for ib in range(self.input_branches):
+                if ob == ib:
                     # 当输入、输出为同一个分支时不做任何处理
                     self.fuse_layers[-1].append(nn.Identity())
-                elif i < j:
+                elif ob < ib:
                     # 当输入分支j大于输出分支i时(即输入分支下采样率大于输出分支下采样率)，
                     # 此时需要对输入分支j进行通道调整以及上采样，方便后续相加
                     self.fuse_layers[-1].append(
                         nn.Sequential(
-                            nn.Conv2d(c * (2 ** j), c * (2 ** i), kernel_size=1, stride=1, bias=False),
-                            nn.BatchNorm2d(c * (2 ** i), momentum=BN_MOMENTUM),
-                            nn.Upsample(scale_factor=2.0 ** (j - i), mode='nearest')
+                            nn.Conv2d(c * (2 ** ib), c * (2 ** ob), kernel_size=1, stride=1, bias=False),
+                            nn.BatchNorm2d(c * (2 ** ob), momentum=BN_MOMENTUM),
+                            nn.Upsample(scale_factor=2.0 ** (ib - ob), mode='nearest')
                         )
                     )
-                else:  # i > j
+                else:  # ob > ib
                     # 当输入分支j小于输出分支i时(即输入分支下采样率小于输出分支下采样率)，
                     # 此时需要对输入分支j进行通道调整以及下采样，方便后续相加
                     # 注意，这里每次下采样2x都是通过一个3x3卷积层实现的，4x就是两个，8x就是三个，总共i-j个
                     ops = []
-                    # 前i-j-1个卷积层不用变通道，只进行下采样
-                    for k in range(i - j - 1):
+                    # 前i-j-1个卷积层不用变通道，只进行下采样。
+                    # 在后面的stage中，是通过多次下采样实现的，因此要根据分支数，例如下采样4倍，要有1次下采样不变通道和1次下采样变通道两层操作。
+                    # 动态追加module中下采样的层。
+                    for k in range(ob - ib - 1):
                         ops.append(
                             nn.Sequential(
-                                nn.Conv2d(c * (2 ** j), c * (2 ** j), kernel_size=3, stride=2, padding=1, bias=False),
-                                nn.BatchNorm2d(c * (2 ** j), momentum=BN_MOMENTUM),
+                                nn.Conv2d(c * (2 ** ib), c * (2 ** ib), kernel_size=3, stride=2, padding=1, bias=False),
+                                nn.BatchNorm2d(c * (2 ** ib), momentum=BN_MOMENTUM),
                                 nn.ReLU(inplace=True)
                             )
                         )
-                    # 最后一个卷积层不仅要调整通道，还要进行下采样
+                    # 最后一个卷积层不仅要调整通道，还要进行下采样. 在输出通道的位置定义下采样层。
                     ops.append(
                         nn.Sequential(
-                            nn.Conv2d(c * (2 ** j), c * (2 ** i), kernel_size=3, stride=2, padding=1, bias=False),
-                            nn.BatchNorm2d(c * (2 ** i), momentum=BN_MOMENTUM)
+                            # 以stage2 中的fuse为例。ib=0，ob=1时，直接下采样。0-branch的输入c=32，因此下采样，卷积参数就是in=32*2^0,out=32*2^1即（32,64）
+                            nn.Conv2d(c * (2 ** ib), c * (2 ** ob), kernel_size=3, stride=2, padding=1, bias=False),
+                            nn.BatchNorm2d(c * (2 ** ob), momentum=BN_MOMENTUM)
                         )
                     )
                     self.fuse_layers[-1].append(nn.Sequential(*ops))
@@ -149,7 +165,9 @@ class StageModule(nn.Module):
         x_fused = []
         for i in range(len(self.fuse_layers)):
             x_fused.append(
+                # 融合后relu
                 self.relu(
+                    # sum 是对fuse中定义的不同module的加过进行融合
                     sum([self.fuse_layers[i][j](x[j]) for j in range(len(self.branches))])
                 )
             )
@@ -158,6 +176,7 @@ class StageModule(nn.Module):
 
 
 class HighResolutionNet(nn.Module):
+    # base_channel就是模型的w32的意思。就是高分辨率branch维持32通道数。
     def __init__(self, base_channel: int = 32, num_joints: int = 17):
         super().__init__()
         # Stem
@@ -169,12 +188,13 @@ class HighResolutionNet(nn.Module):
 
         # Stage1
         downsample = nn.Sequential(
+            # 虽然是downsample，但是k=1，stride=1，没有下采样吧。
             nn.Conv2d(64, 256, kernel_size=1, stride=1, bias=False),
             nn.BatchNorm2d(256, momentum=BN_MOMENTUM)
         )
         self.layer1 = nn.Sequential(
-            Bottleneck(64, 64, downsample=downsample),
-            Bottleneck(256, 64),
+            Bottleneck(64, 64, downsample=downsample),# 通道64之后走了downsample，通道变成了256
+            Bottleneck(256, 64),# 没有downsample，每次bottleneck输出最后都会被变成256通道，这里64是bottleneck头2个conv中用的。
             Bottleneck(256, 64),
             Bottleneck(256, 64)
         )
@@ -194,7 +214,7 @@ class HighResolutionNet(nn.Module):
             )
         ])
 
-        # Stage2
+        # Stage2，执行1次stagemodule
         self.stage2 = nn.Sequential(
             StageModule(input_branches=2, output_branches=2, c=base_channel)
         )
@@ -212,7 +232,7 @@ class HighResolutionNet(nn.Module):
             )
         ])
 
-        # Stage3
+        # Stage3 执行了4次stagemodule
         self.stage3 = nn.Sequential(
             StageModule(input_branches=3, output_branches=3, c=base_channel),
             StageModule(input_branches=3, output_branches=3, c=base_channel),
@@ -234,7 +254,7 @@ class HighResolutionNet(nn.Module):
             )
         ])
 
-        # Stage4
+        # Stage4，3次
         # 注意，最后一个StageModule只输出分辨率最高的特征层
         self.stage4 = nn.Sequential(
             StageModule(input_branches=4, output_branches=4, c=base_channel),
